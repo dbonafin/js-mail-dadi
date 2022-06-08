5f605908d95e5cd64e0d3cc7d1d63e3bb4c6f546
dbonafin <demianbonafina@gmail.com>
2022-06-08 15:41:50 +0200

    mail done

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,4 +1,24 @@
-// Mail
-// Chiedi all’utente la sua email,
-// controlla che sia nella lista di chi può accedere,
-// stampa un messaggio appropriato sull’esito del controllo.
+// ASK THE EMAIL TO THE USER
+const userEmail = prompt ("Digita la tua email");
+
+// LIST OF THE VALID EMAILS FOR THE LOGIN
+const validEmailList = ["tizio@gmail.com", "caio@gmail.com", "pinco@gmail.com", "dbonafin@gmail.com"];
+
+// CHECK IF IT IS PRESENT IN THE LIST OF VALID EMAILS 
+let validationCheck = false;
+
+for ( let i=0 ; i<validEmailList.length; i++ ) {
+    const validEmail = validEmailList[i];
+
+    if (validEmail === userEmail) {
+        validationCheck = true;
+    }
+}
+
+// PRINT THE OUTPUT RESULT WITH AN ALERT
+if (validationCheck) {
+    alert ("Benvenuto nella tua casella di posta elettronica");
+} else {
+    alert ("Accesso negato, prova con un'altra email");
+}
+
